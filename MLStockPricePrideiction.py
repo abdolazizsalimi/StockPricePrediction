582f8f3257fb02aba96956bf5899147a2d518171
Abdolaziz Salimi <67910461+abdolazizsalimi@users.noreply.github.com>
2021-06-02 16:13:49 +0430

    finishe the stock midel

--- MLStockPricePrideiction.py
+++ MLStockPricePrideiction.py
@@ -61,7 +61,7 @@ for i in range(60,len(train_data)):
 x_train,y_train = np.array(x_train), np.array(y_train)
 # reshape the data 
 x_train.shape 
-
+x_train = np.reshape(x_train, (x_train.shape[0] , x_train.shape[1] ,1 ))
 
 #  Bulid the LSTM model 
 model = Sequential()
@@ -73,5 +73,45 @@ model.add(Dense(1))
 
 # compile the model 
 model.compile(optimizer='adam',loss='mean_squared_error' ) 
+# traine the model 
+model.fit(x_train , y_train ,  batch_size=1 , epochs= 1 )
+
+# Creat the testing data set 
+# Creat a new array containing scaled values from index 1543 to 2003 
+test_data = scaled_data[training_data_len-60 : , : ]
+# creat the data sets x_test , y_test 
+x_test = [] 
+y_test = dataset[training_data_len : , :]
+for i in range(60 , len(test_data)): 
+    x_test.append(test_data[i-60:i, 0])  
 
 
+# Convert the data to a numpy array 
+x_test =np.array(x_test)
+# reshape the data 
+x_test = np.reshape(x_test , (x_test.shape[0] , x_test.shape[1] ,1 ))  
+
+# Get the model predictions price values 
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+y_test.shape , predictions.shape
+
+
+# Get the root mean Squared error (RMSE)
+rmes = np.sqrt( np.mean(predictions-y_test )**2) 
+
+
+
+# plot the data 
+train = data[:training_data_len]
+valid = data[training_data_len:] 
+valid['Predictions'] = predictions
+# Visualiize the dasta 
+plt.figure(figsize=(16,8))
+plt.title('Model')
+plt.xlabel('Date' , fontsize = 18 )
+plt.ylabel('Close price USD($)' , fontsize = 18 )
+plt.plot(train["Close"])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'])
+plt.show()
